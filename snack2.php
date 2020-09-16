0f9add89efb4c2592617fdae7e57b8d4983619ad
Mathias <mathias.grandin@gmail.com>
2020-09-16 15:24:36 +0200

    consegna esercizi

--- snack2.php
+++ snack2.php
@@ -1,3 +1,19 @@
 <!-- Snack 2
 Passare come parametri GET name, mail ed age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato -->
+
+<?php
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+if (empty($name) || empty($mail) || empty($age)) {
+  echo 'Compila tutti i campi per proseguire';
+} else if ((strlen($name) <= 3) || ((strpos($mail,'@') == false) && (strpos($mail,'.') == false)) || (!is_numeric($age))) {
+    echo 'Accesso negato';
+} else {
+    echo 'Accesso riuscito';
+}
+
+ ?>
